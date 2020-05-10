900d302feebb3469e605f53605b558d20e1fbcc7
JoshuaLight <j.light.developer@gmail.com>
2020-05-10 23:56:55 +0300

    Fixed comment.

--- candies/cli/args/opt.py
+++ candies/cli/args/opt.py
@@ -19,7 +19,7 @@ class Opt:
         description: A description of an option. Shown in the help page.
         short: A short form of an option.
             Must be non empty and consist of 1 alphabet character.
-        prefix: A string that represents a prefix of a flag.
+        prefix: A string that represents a prefix of an option.
             It should contain only 1 character.
             For example, a value of `+` will allow `++value 2` and `+v 2`
             notation.
