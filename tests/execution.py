922b33ae2e8ca7603444c2de163137d5d9a65057
JoshuaLight <j.light.developer@gmail.com>
2020-05-12 23:14:47 +0300

    Improved comments for `execution` module.

--- tests/execution.py
+++ tests/execution.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable
+from typing import Any, Callable, ContextManager
 import contextlib
 import io
 import sys
@@ -6,9 +6,29 @@ import sys
 
 def execute(cli: Callable[[str], Any],
             with_: str,
-            redirect: Any = None) -> Any:
+            redirect: ContextManager = None) -> Any:
+    """Executes the CLI callable with the specified command line.
 
-    def execute_with(redirect: Any):
+    Supports redirection of either `sys.stdout` or `sys.stderr` for testing
+    purposes.
+
+    Args:
+        cli: A callable that represents a CLI function.
+        with_: A string that represents a command line.
+        redirect: An object that redirects an output into file-like object
+            using context manager protocol.
+
+    Examples:
+        result = execute(cli, '--x')
+        result, err = execute(cli, '--x', redirect_stderr)
+        result, out = execute(cli, '--x', redirect_stdout)
+
+    Todo:
+        * If needed, extend the `redirect` argument to an iterable for
+            redirecting both `sys.stdout` and `sys.stderr`.
+    """
+
+    def execute_with(redirect: ContextManager):
         output = io.StringIO()
 
         with redirect(output):
@@ -28,6 +48,7 @@ def execute(cli: Callable[[str], Any],
 
 
 # Dummy objects that are used only for the following syntax:
-#     execute(cli, with_='args', redirect_stdout)
+#     execute(cli, 'args', redirect_stdout)
+#     execute(cli, 'args', redirect_stderr)
 redirect_stdout = contextlib.redirect_stdout
 redirect_stderr = contextlib.redirect_stderr
