e68a2351cf3ff6823e978bc6a4b740bd2a974ca3
JoshuaLight <j.light.developer@gmail.com>
2020-09-17 15:26:48 +0300

    Changed the version to `0.1.1`.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mints',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Clean and elegant CLI development kit',
